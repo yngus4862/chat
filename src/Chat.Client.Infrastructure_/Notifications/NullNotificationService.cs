9d0ed6b14272b4d189e67722255996cf297d6488
biw <yngus4862@naver.com>
2026-02-26 08:07:53 +0000

    update gitignore

--- src/Chat.Client.Infrastructure_/Notifications/NullNotificationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Chat.Client.Core.Abstractions;
-
-namespace Chat.Client.Infrastructure.Notifications;
-
-public sealed class NullNotificationService : INotificationService
-{
-    public Task ShowMessageToastAsync(string roomName, string sender, string previewText, string deepLink)
-        => Task.CompletedTask;
-}
--- src/Chat.Client.Infrastructure_/Options/ApiOptions.cs
+++
@@ -1,6 +0,0 @@
-namespace Chat.Client.Infrastructure.Options;
-
-public sealed class ApiOptions
-{
-    public string BaseUrl { get; set; } = "http://localhost:8080";
-}
